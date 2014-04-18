f8c9c9416040d1214da4c82e8a5a5cad7d493669
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2014-04-19 00:37:45 +0530

    Add License

--- MyClient.java
+++ MyClient.java
@@ -1,5 +1,20 @@
 /*Client Server Communication via Sockets
-Author- Aaditya Bagga	 Date- 29-09-2013
+
+Copyright (C) 2014  Aaditya Bagga  aaditya_gnulinux@zoho.com
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  any later version.
+
+  This program is distributed WITHOUT ANY WARRANTY;
+  without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 Client*/
 
 import java.io.*;
